7aaa5cd74b4a0e9ac8d093bcdbfb2a28a6a21d26
Rico <168263779+Facialvisitor@users.noreply.github.com.>
2026-03-20 10:16:41 +0100

    add button that creates new grid

--- script.js
+++ script.js
@@ -13,6 +13,47 @@ function createGrid() {
 }
 
 createGrid()
+
+let buttonContainer = document.createElement("div")
+container.before(buttonContainer)
+buttonContainer.classList.add("button")
+
+let button = document.createElement("button")
+button.textContent = "NEW GRID"
+buttonContainer.appendChild(button)
+
+button.addEventListener("click", () => {
+    let input = prompt("Enter new grid:", "")
+
+    if (Number.isNaN(Number(input))) {
+        alert("ERROR: input a number from 0 - 100")
+    }  else if (input > 100){
+        alert("ERROR: input a value less than 100")
+    }  else{
+    newGrid(input)
+ }
+})
+
+function newGrid(input){
+    container.innerHTML = ""
+
+    let n = input;
+    let squareSize = container.offsetWidth / n
+   
+    for (let i = 0; i < input * input; i++){
+        let grid = document.createElement("div")
+        grid.classList.add("grid")
+        grid.style.width = `${squareSize}px`
+        grid.style.height = `${squareSize}px`
+    
+
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "pink"
+        })
+        container.appendChild(grid)
+    }
+
+}
   
 
 
